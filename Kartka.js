0631fde4c1af517e0f3a23ccd8c42f1abcd41580
Nicolas <85492262+Nicolas661@users.noreply.github.com>
2023-02-08 15:12:02 +0100

    Add files via upload

---
+++ Kartka.js
@@ -0,0 +1,36 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+loadSprite("Kartka","kartka.png")
+
+add([
+    sprite("Kartka"),
+    pos(0,0)
+])
+
+loadSprite("Gwiazdka","Gwiazdka1.png")
+
+const gwiazdka = add([
+    sprite("Gwiazdka"),
+    pos(0,100)
+])
+
+onUpdate(()=>{
+
+    if(gwiazdka.pos.x<500)
+{
+    gwiazdka.pos.x=gwiazdka.pos.x + 1.5
+    gwiazdka.pos.y=gwiazdka.pos.y + 0.2
+
+}
+
+else gwiazdka.pos.x=0
+})
+
+loadSound("muzyka","dzisiaj.mp3")
+
+onKeyPress("space",()=>{
+    play ("muzyka")
+})
